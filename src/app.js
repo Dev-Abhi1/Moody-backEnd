5d1cdd56b1ca4d38002597cf08edc29c1189d6e6
Abhishek Choudhary <ac9657449@gmail.com>
2025-08-05 17:14:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-const express = require("express")
-const songRoutes = require("./routes/song.route")
-const cors = require("cors")
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use("/",songRoutes)
+const express = require("express");
+const songRoutes = require("./routes/song.route");
+const cors = require("cors");
 
-module.exports = app
+const app = express();
+
+app.use(cors({
+  origin: "https://comforting-selkie-d4db40.netlify.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+app.use("/", songRoutes);
+
+module.exports = app;
